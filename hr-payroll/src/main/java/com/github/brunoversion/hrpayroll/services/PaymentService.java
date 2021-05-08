3969a7eacea8fea3e789ae28612a0f8a1e59fcf5
brunoversion <bas.80@hotmail.com>
2021-05-08 20:36:46 -0300

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/github/brunoversion/hrpayroll/services/PaymentService.java
@@ -0,0 +1,14 @@
+package com.github.brunoversion.hrpayroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.github.brunoversion.hrpayroll.entities.Payment;
+
+@Service
+public class PaymentService {
+	
+	public Payment getPayment(long workerId, int days) {
+		return new Payment("Bob", 200.0, days);
+	}
+
+}
