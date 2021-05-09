6ed318ff22ad78fb51aca9e758502c747ac18e15
brunoversion <bas.80@hotmail.com>
2021-05-09 09:43:57 -0300

    Project hr-config-server created

---
+++ hr-config-server/src/test/java/com/github/brunoversion/hrconfigserver/HrConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.brunoversion.hrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
